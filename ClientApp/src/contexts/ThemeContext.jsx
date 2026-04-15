5d5c4fbc7c9fe67b4b4f94382f53f0e30104ad45
Eyob Begashaw Ayele <eyobbegashaw075@gmail.com>
2026-04-15 08:49:01 -0700

    Update ThemeContext.jsx

--- ClientApp/src/contexts/ThemeContext.jsx
+++ ClientApp/src/contexts/ThemeContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
 import { useColorScheme } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
+    
 const ThemeContext = createContext();
 
 export const useTheme = () => useContext(ThemeContext);
