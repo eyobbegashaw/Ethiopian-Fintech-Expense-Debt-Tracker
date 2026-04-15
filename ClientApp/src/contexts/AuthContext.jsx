32657d3a559ec10cfa615a126adb6d39349a3384
Eyob Begashaw Ayele <eyobbegashaw075@gmail.com>
2026-04-15 08:48:49 -0700

    Update AuthContext.jsx

--- ClientApp/src/contexts/AuthContext.jsx
+++ ClientApp/src/contexts/AuthContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import api from '../services/api';
-
+    
 const AuthContext = createContext({});
 
 export const useAuth = () => useContext(AuthContext);
